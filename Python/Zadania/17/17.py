bfe0d7c0db374774422a288a2e05a69f6e1e29cf
PlayKW <k.wieczaszek@gmail.com>
2022-03-29 13:26:37 +0200

    29.03-inf-py

--- Python/Zadania/17/17.py
+++ Python/Zadania/17/17.py
@@ -1,19 +1,50 @@
-﻿#import
+﻿# Import
+from ctypes.wintypes import CHAR
 import random
 
-#preparation
+# Preparation
 wrds = []
 usedChars = []
 for x in range(1,21):
     wrds.append(f"slowko{x}")
 
 pattern = random.choice(wrds)
-screen = ""
+label = ""
 
 for c in pattern:
-    screen += '-'
+    label += '-'
 
-#display
-print("*======================*\n")
-print(screen)
-print(f"Used: {usedChars}")
+
+# Game logic
+while label != pattern:
+
+    # Display
+    display = ""
+    for c in label:
+        display += c + ' '
+    
+    print("=-------------------------------=\n")
+    print(display)
+    print("Used: ",)
+
+    # Input
+    char = input("Given letter: ")
+
+    # Letter select
+    selectedAny = False
+    for i in range(0,len(pattern)):
+        if char == pattern[i]:
+
+            # Label index replace
+            s = list(label)
+            s[i] = char
+            label = "".join(s)
+
+            # List insert
+            usedChars.append(char)
+            selectedAny = True
+
+    # Fail message (is any letter selected?)
+    if not selectedAny:
+        print(f"No matches found for letter '{char}'")
+
