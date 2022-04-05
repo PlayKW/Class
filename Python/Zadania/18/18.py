2a10233a1a717b43167a4cc37d27f8e725ec1cb2
PlayKW <k.wieczaszek@gmail.com>
2022-04-05 13:28:48 +0200

    inf-5.04-py

---
+++ Python/Zadania/18/18.py
@@ -0,0 +1,33 @@
+﻿# Import
+from operator import contains
+
+# Preparation
+letterSet = list("abcdefghijklmnopqrstuvwxyz")
+setLen = len(letterSet)
+result = ""
+
+# Initial input
+inp = input("Type the message: ")
+key = int(input("Give the key: "))
+
+### Logic
+# Word letter loop
+for c in inp:
+
+    isCap = c.isupper()
+    newC = c.lower()
+    # Is any translation?
+    if contains(letterSet,c):
+        # Translate - new letter
+        for i in range(0, setLen):
+            if letterSet[i] == c:
+                index = (i + key) % setLen
+                result += letterSet[index]
+    # Is no translation?
+    else:
+        result += c
+
+# End output
+print("====================================")
+print("* Message:\n")
+print(result)
