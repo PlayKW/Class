794401f5829933fec0fbff0a6e923e1d947c1dea
PlayKW <91133268+PlayKW@users.noreply.github.com>
2022-06-02 19:20:54 +0200

    Update 17.py

--- Python/Zadania/17/17.py
+++ Python/Zadania/17/17.py
@@ -1,20 +1,27 @@
 # Import
-from ctypes.wintypes import CHAR
-from operator import contains
 import random
 
+# Lista jest pusta, bo na dole zostanie wygenerowane 20 kolejnych słów.
+# Można tą pętlę usunąć i wypisać w liście słowa - też będzie działać.
+# Przykładowa lista : ["komputer","python","programowanie","algorytm","skrypt","składnia","obkiekt","monitor","zasilacz","kamera","procesor","pamięć ram","słuchawki","mysz","karta graficzna","wentylator","pendrive","dysk","płyta","chmura"]
+
 ### Preparation
-wrds = []
+wrds = [str]
 usedChars = []
+
 for x in range(1,21):
     wrds.append(f"slowko{x}")
 
-pattern = random.choice(wrds)
+
+pattern = random.choice(wrds).lower()
 label = ""
 tries = 0
 
 for c in pattern:
-    label += '-'
+    if not c == ' ':
+        label += '-'
+    else:
+        label += ' '
 
 # Visual elements
 ftr = "=-------------------------------=\n"
@@ -36,15 +43,19 @@ while label != pattern:
     print("Used:",usedChars)
 
     # Input
-    inp = input("Question: ")
+    inp = input("Question: ").lower()
+    if inp in usedChars:
+            print(f"You have already asked about this letter!")
+            continue
 
     # Letter select
     if len(inp) == 1:
+
+        # List insert
+        usedChars.append(inp)
+
         # Is lettern in a pattern?
         if inp in pattern:
-            
-            # List insert
-            usedChars.append(inp)
 
             # Replace loop
             for i in range(0,len(pattern)):
@@ -68,11 +79,10 @@ while label != pattern:
         if inp == pattern:
             break
         else:
-            print(f"Word {inp} is incorrect! Try again.")
-
+            print(f"Word '{inp}' is incorrect!")
 
 ### End
 print(ftr)
 print("Congratulations!")
-print(f"The chosen word was {pattern}.")
+print(f"The chosen word was '{pattern}'.")
 print(f"It took you {tries} tries to guess it!")
